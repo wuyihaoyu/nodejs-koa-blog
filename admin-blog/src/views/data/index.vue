58911c5ceb97aaa18269eb263893b1e2c99b0bda
林浩裕 <1490227844@qq.com>
2022-03-12 01:59:09 +0800

    fix:增加了饼状图

--- admin-blog/src/views/data/index.vue
+++ admin-blog/src/views/data/index.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-  <div id="main" style="width: 600px;height:400px;"></div>
+  <div id="main" style="width: 500px;height:400px;margin-bottom:100px"></div>
+  <div id="main2" style="width: 1000px;height:400px;"></div>
 </div>
 </template>
 
@@ -20,6 +21,9 @@ export default {
        list:[],
        names:[],
        num:[],
+       browse:[],
+       browsenum:[],
+       browsetitle:[],
       searchForm: {
         id: '',
         title: '',
@@ -39,12 +43,12 @@ export default {
       try {
         const res2 = await list(this.searchForm)
         this.list = res2.data.data
-      
-        // const res = await getCategoryList()
-        // this.categoryList = res.data.data
           
          this.Separatedata()
          this.showChart()
+        
+         this.getbrowse()
+         this.showdata()
 
 
       } catch (err) {
@@ -55,10 +59,7 @@ export default {
     },
     Separatedata(){
       let titleList = this.list.map(item => {
-      return item.category_info.name
-        })
-      console.log(this.getRepeatNum(titleList))
-      // {生活: 4, 前端JavaScript: 2}
+      return item.category_info.name  })
       for(var i in this.getRepeatNum(titleList)){
               this.num.push(this.getRepeatNum(titleList)[i])
               this.names.push(i)
@@ -74,12 +75,22 @@ export default {
         return obj; 
     },
 
+    getbrowse(){
+        for(var i=0;i<this.list.length;i++){
+          //  this.browsenum.push(this.list[i].browse)
+          //  this.browsetitle.push(this.list[i].title)
+           this.browse.push({value:this.list[i].browse,name:this.list[i].title})
+        }
+        // console.log(this.browse)
+        // console.log(this.browsetitle)
+    },
+
     showChart(){
           var myChart = echarts.init(document.getElementById('main'));
         // 指定图表的配置项和数据
           var option = {
             title: {
-              text: '文章类型数量统计'
+              text: '文章--类型数量统计'
             },
             tooltip: {},
             legend: {
@@ -99,6 +110,46 @@ export default {
           };
       // 使用刚指定的配置项和数据显示图表。
       myChart.setOption(option);
+    },
+
+    showdata(){
+      var chartDom = document.getElementById('main2');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+  title: {
+    text: 'Website--文章访问量 ',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '60%',
+      data: this.browse,
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
+      
     }
 
   }
